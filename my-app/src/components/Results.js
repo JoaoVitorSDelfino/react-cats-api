46c3f88f6e98a8da262799faff06353d79ba5273
João Vitor Schiavinato Delfino <joao.delfino.pb@compasso.com.br>
2024-04-13 18:50:49 -0300

    feat: mostrar todos os resultado da pesquisa

--- my-app/src/components/Results.js
+++ my-app/src/components/Results.js
@@ -5,8 +5,14 @@ const Results = ({ data }) => {
   return (
     <div>
       {data.map((cat, index) => (
-        <div key={index}>
-          <p>{cat.name}</p>
+        <div key={index} className='catInfo'>
+          <h2>{cat.name}</h2>
+          <p> Playfulness (scale 1 to 5): {cat.playfulness} </p>
+          <p> Length: {cat.length} </p>
+          <p> Weight: {cat.min_weight} - {cat.max_weight} pounds</p>
+          <p> Life expectancy: {cat.min_life_expectancy} - {cat.max_life_expectancy} years</p>
+          <img src={cat.image_link} alt='cat'/>
+
         </div>
       ))}
     </div>
