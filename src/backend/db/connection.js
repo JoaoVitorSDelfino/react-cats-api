d664c717f49fbc7bdc88fa43be2d8d434b24959c
JoaoVitorSDelfino <103132209+JoaoVitorSDelfino@users.noreply.github.com>
2024-08-31 12:52:59 -0300

    refactor: config de conexão de pools do sqlite

--- src/backend/db/connection.js
+++ src/backend/db/connection.js
@@ -2,7 +2,13 @@ const Sequelize = require('sequelize')
 
 const sequelize = new Sequelize({
     dialect: 'sqlite',
-    storage: '../db/app.sqlite'
+    storage: '../db/app.sqlite',
+    pool: {
+        max: 5,         // Número máximo de conexões no pool
+        min: 0,         // Número mínimo de conexões no pool
+        acquire: 30000, // Tempo máximo, em milissegundos, que o pool tentará obter uma conexão antes de gerar um erro
+        idle: 10000,    // Tempo máximo, em milissegundos, que uma conexão pode permanecer inativa antes de ser liberada
+    }
 })
 
 module.exports = sequelize
